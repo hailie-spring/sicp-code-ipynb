bcee7bc5f14346e5192ceaa7628fa7ba7e2c7b11
unknown <hailie_chou@qq.com>
2024-05-16 15:46:18 +0800

    update

---
+++ chapter1/base_definition.py
@@ -0,0 +1,24 @@
+def square(x):
+    return x * x
+
+
+def average(x, y):
+    return (x + y) / 2
+
+
+def good_enough(guess, x):
+    return abs(square(guess) - x) < 0.001
+
+
+def improve_guess(guess, x):
+    return average(guess, x / guess)
+
+
+def sqrt_iter(guess, x):
+    if good_enough(guess, x):
+        return guess
+    return sqrt_iter(improve_guess(guess, x), x)
+
+
+def sqrt(x):
+    return sqrt_iter(1, x)
