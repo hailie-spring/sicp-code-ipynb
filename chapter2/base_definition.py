5d2d076da1d660b5f71ff5dfb9fe8127c9359716
unknown <hailie_chou@qq.com>
2024-06-04 17:25:29 +0800

    update 2.2.1

--- chapter2/base_definition.py
+++ chapter2/base_definition.py
@@ -47,3 +47,44 @@ def set_car(z, new_value):
 def set_cdr(z, new_value):
     z("set_cdr")(new_value)
     return z
+
+
+# def list(*args):
+#     h = None
+#     r = None
+#     for arg in args:
+#         if r is None:
+#             r = cons(arg, None)
+#             h = r
+#         else:
+#             t = cons(arg, None)
+#             set_cdr(r, t)
+#             r = t
+#     return h
+
+# def display_list(lst):
+#     def iter(text, lst):
+#         if lst is None:
+#             return text
+#         return iter(text + " " + str(car(lst)), cdr(lst))
+
+#     text = iter("", lst)
+#     print("(" + text + " )")
+
+
+def list(*args):
+    if args == ():
+        return None
+    return cons(args[0], list(*args[1:]))
+
+
+def display_list(lst):
+
+    def sub_display_list(lst):
+        if lst is None:
+            return ""
+        return str(car(lst)) + " " + sub_display_list(cdr(lst))
+    
+    text = sub_display_list(lst)
+    print("( " + text + ")")
+
